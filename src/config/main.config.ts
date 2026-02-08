29e0a1ce4a5962ba315677d9cb86722ab6565ffe
DeadInsideDi <matvei778990@gmail.com>
2026-02-08 22:21:45 +0300

    init data config

---
+++ src/config/main.config.ts
@@ -0,0 +1,8 @@
+export const CONFIG = {
+	company: 'MatSen',
+	name: 'Matvei Senckevich',
+	location: 'Volgograd, Russia',
+	phone: '+79178398390',
+	email: 'matvei778990@gmail.com',
+	telegram: 'https://t.me/MatSen1',
+} as const
